79f82fba81042881021a32686159bf629d464e90
Raju <ashfaqraju27@gmail.com>
2023-08-14 02:13:50 +0600

    banner part complete

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import image  from '../assets/photo.png';
-import {FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaWhatsapp} from 'react-icons/fa';
+import {FaGithub, FaInstagram, FaLinkedin, FaWhatsapp} from 'react-icons/fa';
 import {TypeAnimation} from 'react-type-animation';
 import {motion} from 'framer-motion';
 import {fadeIn} from '../variants';
@@ -12,8 +12,22 @@ const Banner = () => {
         <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-6' > 
          {/* text */}
          <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[30px] font-bold leading-[1] lg:text-[60px]'>AsfaK Hossain Raju</h1>
-            <div className='mb-6 text-[20px] lg:text-[40px] font-secondary
+            <motion.h1 
+              variants={fadeIn('up', 0.3)} 
+              initial='hidden'
+              whileInView={'show'} 
+              viewport={{once: false, amount: 0.7}} 
+              className='text-[30px] font-bold leading-[1] lg:text-[60px]'
+             >
+              AsfaK Hossain Raju
+              </motion.h1>
+            
+            <motion.div 
+              variants={fadeIn('up', 0.5)} 
+              initial='hidden'
+              whileInView={'show'} 
+              viewport={{once: false, amount: 0.7}} 
+              className='mb-6 text-[20px] lg:text-[40px] font-secondary
               font-semibold uppercase leading-[1]'>
               <span className='mr-4'>I am a </span>
               <TypeAnimation 
@@ -28,44 +42,61 @@ const Banner = () => {
                 wrapper='span'
                 repeat={Infinity}
               />
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.7)} 
+              initial='hidden'
+              whileInView={'show'} 
+              viewport={{once: false, amount: 0.7}} 
+              className='mb-8 max-w-lg mx-auto lg:mx-0'> 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Magnam, beatae mollitia! Aperiam sit reiciendis, quia officiis 
                 quo praesentium aliquam eligendi soluta provident temporibus 
                 blanditiis minus eaque autem voluptatem voluptate veniam!
-            </p>
+            </motion.p>
             
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
-              <button className='btn btn-lg'>Contact Me</button>
-              <a href="#" className='text-gradient btn-link'>
-                My Portfolio
-              </a>
-            </div>
+            <motion.div
+             variants={fadeIn('up', 0.9)} 
+             initial='hidden'
+             whileInView={'show'} 
+             viewport={{once: false, amount: 0.7}}
+             className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+                <button className='btn btn-lg'>Contact Me</button>
+                <a href="#" className='text-gradient btn-link'>
+                  My Portfolio
+                </a>
+            </motion.div>
             {/* social media */}
-            <div className='flex text-[20px] gap-x-16 max-w-max mx-auto lg:mx-0'>
-              <a href="#">
-                <FaGithub/>
-              </a>
-              <a href="#">
-                <FaLinkedin/>
-              </a>
-              <a href="#">
-                <FaInstagram/>
-              </a>
-              <a href="#">
-                <FaFacebook/>
-              </a>
-              <a href="#">
-                <FaWhatsapp/>
-              </a>
-            </div>
+            <motion.div 
+              variants={fadeIn('up', 1)} 
+              initial='hidden'
+              whileInView={'show'} 
+              viewport={{once: false, amount: 0.7}}
+              className='flex text-[20px] gap-x-16 max-w-max mx-auto lg:mx-0'>
+                <a href="#">
+                  <FaGithub/>
+                </a>
+                <a href="#">
+                  <FaLinkedin/>
+                </a>
+                <a href="#">
+                  <FaInstagram/>
+                </a>
+                <a href="#">
+                  <FaWhatsapp/>
+                </a>
+            </motion.div>
          </div>
         
         {/* img */}
-         <div className='hidden lg:flex flex-2 max-w-[320px] lg:max-w-[480px]'>
+         <motion.div
+          variants={fadeIn('down', 0.5)} 
+          initial='hidden'
+          whileInView={'show'}
+          className='hidden lg:flex flex-2 max-w-[320px] lg:max-w-[480px]'>
           <img src={image} style={{width: '250px', backgroundColor:'gray',borderRadius:'50px'}} alt="" />
           
-         </div>
+         </motion.div>
 
         </div>
       </div>
