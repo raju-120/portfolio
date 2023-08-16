1d0053f5c79fe78d8289a13d5e3023f2e89bca90
Raju <ashfaqraju27@gmail.com>
2023-08-16 16:31:03 +0600

    complete

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,9 @@ import React from 'react';
 import img1 from '../assets/portfolio-img1.png';
 import img2 from '../assets/portfolio-img2.png';
 
+import {FaGithub} from 'react-icons/fa';
+import { BiLinkExternal } from "react-icons/bi";
+
 const Work = () => {
   return (
     <section className='min-h-[85vh] lg:min-h-[78vh] mt-24 lg:mb-80' id='work'>
@@ -25,21 +28,35 @@ const Work = () => {
             
               <div className='group relative overflow-hidden border-2
                border-white/50 rounded-xl' >
-                {/* overlay */}
+                
                 <div className='group-hover:bg-black/70 w-full h-full absolute
                 z-40 transition-all duration-300'></div>
-                {/* img */}
+                
                 <img src={img1} className='group-hover:scale-125 transition-all duration-500' alt="img1" />
-                {/* preTitle */}
+                
                 <div className='absolute -bottom-full left-12
                  group-hover:bottom-24 transition-all duration-500 z-50'>
                   <span className='text-gradient'>Web-Development</span>
                 </div>
-                {/* title */}
+                
                 <div className='absolute -bottom-full left-12
                  group-hover:bottom-14 transition-all duration-700 z-50'>
                   <span className='text-3xl text-white'>Doctor's Portal</span>
                 </div>
+                <div className='absolute -bottom-full left-12
+                 group-hover:bottom-10 transition-all duration-1000 z-50 flex'>
+                  <div>
+                  <span><a href="https://github.com/raju-120/doctor-portal-client">
+                      <FaGithub/>
+                    </a></span>
+                  </div>
+                  <div className='ml-5'>
+                  <span><a href="https://dental-portal25896.netlify.app">
+                      <BiLinkExternal/>
+                    </a></span>
+                  </div>
+                </div>
+                
               </div>
 
             </div>
@@ -59,9 +76,23 @@ const Work = () => {
                 {/* title */}
                 <div className='absolute -bottom-full left-12
                  group-hover:bottom-14 transition-all duration-700 z-50'>
-                  <span className='text-3xl text-white'>Resell</span>
+                  <span className='text-3xl text-white'>Resell Market</span>
+                </div>
+                <div className='absolute -bottom-full left-12
+                 group-hover:bottom-10 transition-all duration-1000 z-50 flex'>
+                  <div>
+                  <span><a href="https://github.com/raju-120/used-phone-market-client">
+                      <FaGithub/>
+                    </a></span>
+                  </div>
+                  <div className='ml-5'>
+                  <span><a href="https://resellmarket.netlify.app">
+                      <BiLinkExternal/>
+                    </a></span>
+                  </div>
                 </div>
               </div>
+
               <div className='group relative overflow-hidden border-2
                border-white/50 rounded-xl' >
                 {/* overlay */}
@@ -78,6 +109,19 @@ const Work = () => {
                 <div className='absolute -bottom-full left-12
                  group-hover:bottom-14 transition-all duration-700 z-50'>
                   <span className='text-3xl text-white'>Doctor's Portal</span>
+                </div>
+                <div className='absolute -bottom-full left-12
+                 group-hover:bottom-10 transition-all duration-1000 z-50 flex'>
+                  <div>
+                  <span><a href="https://github.com/raju-120/Genius-Car-Client">
+                      <FaGithub/>
+                    </a></span>
+                  </div>
+                  <div className='ml-5'>
+                  <span><a href="https://github.com/raju-120">
+                      <BiLinkExternal/>
+                    </a></span>
+                  </div>
                 </div>
               </div>
             </div>
