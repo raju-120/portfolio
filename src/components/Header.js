4c15a7f0742394253c401bbbdb6821aeef495d2d
Raju <ashfaqraju27@gmail.com>
2023-09-10 12:23:16 +0600

    changed on header folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import resumePDF from "../assets/raju'sResume.pdf"; // Update the path to your PDF file
 import logo from '../assets/logo-removebg.png';
 
 const Header = () => {
-  return(
-     
+  return (
     <header className='py-8'>
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
           <img src={logo} className='w-20' alt="" />
 
-          <button className='btn btn-primary'>
-            <a 
-              href="https://drive.google.com/file/d/1pzml79V1JUp7dsizpc_gyKzh-tap4qUC/view?usp=sharing"
-              >
-                Resume
-            </a>
-          </button>
+          {/* Resume download link */}
+          <a
+            href={resumePDF}
+            download="raju'sResume.pdf"
+            className='btn btn-primary'
+          >
+            Download Resume
+          </a>
         </div>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
